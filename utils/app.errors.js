dc124c838b8a9aee07d5521987cebbca136238e4
Minamaher-sw <mosadefmena185@gmail.com>
2025-07-01 22:30:21 +0300

    Refactor AppErrors class to improve error handling and add static methods for common HTTP errors

--- utils/app.errors.js
+++ utils/app.errors.js
@@ -1,13 +1,46 @@
-class AppErrors extends Error{
-    constructor(){
-        super()
+class AppErrors extends Error {
+    constructor(message, statusCode, httpStatusText) {
+        super(message);
+        this.statusCode = statusCode || 500;
+        this.httpStatusText = httpStatusText || "error";
+        Error.captureStackTrace(this, this.constructor);
     }
-    createError(message , statusCode , httpStatusText){
-        this.message = message;
-        this.statusCode = statusCode;
-        this.httpStatusText = httpStatusText;
-        return this;
+
+    static createError(message, statusCode = 500, httpStatusText = "error") {
+        return new AppErrors(message, statusCode, httpStatusText);
+    }
+
+    static badRequest(message = "Bad Request") {
+        return new AppErrors(message, 400, "fail");
+    }
+
+    static unauthorized(message = "Unauthorized") {
+        return new AppErrors(message, 401, "fail");
+    }
+
+    static forbidden(message = "Forbidden") {
+        return new AppErrors(message, 403, "fail");
+    }
+
+    static notFound(message = "Not Found") {
+        return new AppErrors(message, 404, "fail");
+    }
+
+    static conflict(message = "Conflict") {
+        return new AppErrors(message, 409, "fail");
+    }
+
+    static unprocessableEntity(message = "Unprocessable Entity") {
+        return new AppErrors(message, 422, "fail");
+    }
+
+    static internal(message = "Internal Server Error") {
+        return new AppErrors(message, 500, "error");
+    }
+
+    static serviceUnavailable(message = "Service Unavailable") {
+        return new AppErrors(message, 503, "error");
     }
 }
 
-export default new AppErrors();
+export default AppErrors;
