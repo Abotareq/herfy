b1f405c953f7ace2505b94926c951dce89de9746
mohammed6699 <mohammedahmed.refaat@gmail.com>
2025-07-02 17:35:50 +0300

    add the category controller and route

---
+++ controllers/category.controller.js
@@ -0,0 +1,65 @@
+import Category from "../models/categoryModel.js";
+import StatusCodes from '../utils/status.codes';
+import httpStatus from '../utils/http.status.message'
+import StatusCodes from './../utils/status.codes';
+import { getAllCategoty } from './category.controller';
+
+// get all category
+export const getAllCategoty = async(req, res) => {
+    // pagination 
+    try {
+    const query = req.query;
+    const limit = query.limit;
+    const page = query.page
+    const end = (page - 1) * limit
+    const allCategories = await Category.find().limit(limit).skip(end);
+    if(!allCategories){
+        res.status(StatusCodes.UNAUTHORIZED).json({status: httpStatus.ERROR, data:{message: "Unothoriaed"}})
+    }
+    res.status(StatusCodes.ACCEPTED).json({status: httpStatus.SUCCESS, data: {allCategories}})
+    } catch (err) {
+        res.status(StatusCodes.UNAUTHORIZED).json({data: {status: err, message: "unothorized user"}})   
+    }
+}
+// add category
+export const addNewCategory = async (req, res) => {
+    if(!req.decoded){
+        res.status(StatusCodes.BAD_REQUEST).json({status: httpStatus.ERROR, data:{message: 'Unothorized user'}})
+    }
+    try {
+        const {name, parent, image} = req.body;
+        const newCategory = await Category.create({
+            name,
+            parent,
+            image
+        })
+        res.status(StatusCodes.ACCEPTED).json({status: httpStatus.SUCCESS, data:{newCategory}})
+    } catch (error) {
+        res.status(StatusCodes.UNAUTHORIZED).json({status: error,data: {message: "Failed to add new Category"}})
+    }
+}
+// get category by id
+export const getCategotyById = async(req, res) => {
+    try {
+        const catId = req.params.id
+    const category = await Category.findById(catId);
+    if(category){
+        res.status(StatusCodes.NOT_FOUND).json({status: httpStatus.ERROR, data: {message: 'Not match Category'}});
+    }
+    res.status(StatusCodes.ACCEPTED).json({status: httpStatus.SUCCESS, data:{category}})
+    } catch (error) {
+        res.status(StatusCodes.UNAUTHORIZED).json({data: {status: error, message: 'failed to get category'}})
+    }
+}
+export const deleteCategory = async(req, res) => {
+    const categoryId = req.params.id
+    try {
+        const deletedCategory = Category.findByIdAndDelete(categoryId)
+        if(!deletedCategory){
+            res.status(StatusCodes.BAD_REQUEST).json({status: httpStatus.ERROR, data:{message: "Can't get this category"}});
+        }
+        res.status(StatusCodes.OK).json({status: httpStatus.SUCCESS, data:{message: "The Category deleted"}})
+    } catch (error) {
+         res.status(StatusCodes.UNAUTHORIZED).json({data: {status: error, message: 'failed to get category'}})
+    }
+}
---
+++ routes/category.route.js
@@ -0,0 +1,12 @@
+import {getAllCategoty, 
+        addNewCategory,
+        getCategotyById,
+        deleteCategory } from "../controllers/category.controller.js";
+
+import express from 'express';
+const categoryRouter = express.Router;
+
+categoryRouter.get('/', getAllCategoty)
+categoryRouter.get('/:id', getCategotyById)
+categoryRouter.delete('/:id', deleteCategory)
+categoryRouter.post('/:id', addNewCategory)
