dc7dc09dfec9f00169208e7f295c8378b25d4444
mohammed6699 <mohammedahmed.refaat@gmail.com>
2025-07-12 20:26:31 +0300

    adding the filter controller to category, coupn route, products route

--- routes/review.route.js
+++ routes/review.route.js
@@ -2,12 +2,14 @@ import express from 'express';
 import {addNewReview, getAllReviews, updateReviews, deleteReview, deleteReviewByAdmin} from '../controllers/review.controller.js';
 import { checkRole,requireAuth } from '../auth/auth.middleware.js';
 import userRole from '../utils/user.role.js';
+import { filterReviewByProducts } from '../controllers/filter.controller.js';
 const reviewRouter = express.Router();
 reviewRouter.post('/', requireAuth,checkRole([userRole.CUSTOMER]), addNewReview);
 reviewRouter.get('/',requireAuth,checkRole([userRole.ADMIN]),  getAllReviews);
 reviewRouter.patch('/:id',requireAuth,checkRole([userRole.CUSTOMER]),  updateReviews);
 reviewRouter.delete('/:id', requireAuth,checkRole([userRole.VENDOR, userRole.CUSTOMER]), deleteReview);
 reviewRouter.delete('/:id', requireAuth,checkRole([userRole.ADMIN]), deleteReviewByAdmin);
+reviewRouter.get('/filter', requireAuth, checkRole([userRole.ADMIN]), filterReviewByProducts)
 // reviewRouter.delete('/:id', requireAuth, checkRole([userRole.ADMIN]),deletUserByUser)
 //REVIEW FOR CERTAIN PRODUCT OR CERTAIN STORE
 export default reviewRouter
